665c56a1afb084eaedcaa234b591563f5f59db30
alexis <alexis@education.com>
2020-01-21 20:45:28 -0500

    changed main layout, made topics expandable

--- src/components/AccordionHeader/AccordionHeader.js
+++ src/components/AccordionHeader/AccordionHeader.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import './AccordionHeader.scss'
+import './AccordionHeader.scss';
 
 class AccordionHeader extends React.Component {
+
+  handleClick = () => {
+    const {toggleFunction} = this.props
+    
+    if(toggleFunction){
+      toggleFunction()
+    }
+  }
+
   render() {
     return (
-      <div className={`accordion-header ${this.props.display ? 'accordion-header-active' : null}`}>
+      <div onClick = {this.handleClick} className={`accordion-header ${this.props.display ? 'accordion-header-active' : null}`}>
         <div className="content">
           {this.props.children}
         </div>
