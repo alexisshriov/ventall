919ebe30220cc9a2b168d95a1d0a408b18063b27
alexis <alexis@education.com>
2020-03-18 17:13:08 -0500

    Revert "changed main layout, made topics expandable"
    This reverts commit 665c56a1afb084eaedcaa234b591563f5f59db30.

--- src/components/AccordionHeader/AccordionHeader.js
+++ src/components/AccordionHeader/AccordionHeader.js
@@ -1,19 +1,10 @@
 import React from 'react';
-import './AccordionHeader.scss';
+import './AccordionHeader.scss'
 
 class AccordionHeader extends React.Component {
-
-  handleClick = () => {
-    const {toggleFunction} = this.props
-    
-    if(toggleFunction){
-      toggleFunction()
-    }
-  }
-
   render() {
     return (
-      <div onClick = {this.handleClick} className={`accordion-header ${this.props.display ? 'accordion-header-active' : null}`}>
+      <div className={`accordion-header ${this.props.display ? 'accordion-header-active' : null}`}>
         <div className="content">
           {this.props.children}
         </div>
--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,24 +2,17 @@ import React from 'react';
 import CodeLine from '../CodeLine/CodeLine';
 import AccordionHeader from '../AccordionHeader/AccordionHeader';
 import AccordionBody from '../AccordionBody/AccordionBody';
-import LineInfo from '../CodeLine/CodeLine';
 import './Page.scss';
 
 class Page extends React.Component {
   state = {
-    expandHypCont1: false,
+    expandHypCont1: true,
     showBody1: false,
-    selectedLineIndex: -1,
-    showVariablesContent: true
-
+    selectedLineIndex: -1 
   }
 
   expandHyperContent = () => {
     this.setState({ showBody1: !this.state.showBody1 })
-  }
-
-  expandVariableContent = () => {
-    this.setState({ showVariablesContent: !this.state.showVariablesContent })
   }
 
   onLineSelect = (index) => {
@@ -29,61 +22,57 @@ class Page extends React.Component {
   renderInfo = (lineIndex) => {
     switch(lineIndex){
       case 1:
-        return(       
+        return(
           <div>
-            <AccordionHeader display={this.state.expandHypCont1}>
-              <div className="line-info">
-                The const keyword creates a <b style={{ color: '#15aabf' }} onClick={this.expandHyperContent}>read-only</b> reference to a value.
-              </div>      
+            <AccordionHeader toggleFunction={this.onClick} display={this.state.expandHypCont1}>
+              The const keyword creates a <b style={{ color: '#15aabf' }} onClick={this.expandHyperContent}>read-only</b> reference to a value.
             </AccordionHeader>
-            <AccordionBody display={this.state.showBody1}>
-              <div className="line-info">
-                A <b onClick={this.expandHyperContent} style={{ color: '#15aabf' }}>read-only</b> value is a value that once asigned cannot be changed later on.
-                {'If you try to re-asign it it will throw erros:'}<br /><br />
-                <b style={{ color: '#15aabf' }}>Example:</b> <br />
-                {'const a = 0;'}<br />
-                {'a = 1;'}<span style={{ color: 'brown' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//  this will throw an error.</span>
-               </div>    
+            <AccordionBody display={this.state.showBody1 && this.state.expandHypCont1} >
+              A <b style={{ color: '#15aabf' }}>read-only</b> value is a value that once asigned cannot be changed later on.
+              {'If you try to re-asign it it will throw erros:'}<br /><br />
+              <b style={{ color: '#15aabf' }}>Example:</b> <br />
+              {'const a = 0;'}<br />
+              {'a = 1;'}<span style={{ color: 'brown' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//  this will throw an error.</span>
             </AccordionBody>
           </div>    
         )
+        break;
       case 2: 
         return(
           <div>
              <AccordionHeader toggleFunction={this.onClick} display={this.state.expandHypCont1}>
-              <div className="line-info">
-                  {'this is another explanation about  a secondary line'}
-              </div>
+              {'this is another explanation about  a secondary line'}
             </AccordionHeader>
           </div>
         )
+        break;
     }
   }
   
   render() {
     return (
       <div className="page">
-        <AccordionHeader toggleFunction={this.expandVariableContent} display={this.state.expandHypCont1}>
+        <div class="grid-container">
           <div class="grid-item header-item">
             Variables:
           </div>
-        </AccordionHeader>
-        <AccordionBody display={this.state.showVariablesContent} >
-          <div class="grid-container">
-            <div class="grid-item info-item">
-              You can think of a variable as a container in memory where you can store a value.
-            </div>
-            <div class="grid-item code-panel">
-              <CodeLine onLineSelect={this.onLineSelect} index={1} link>{'let name = "Jose";'}</CodeLine>
-              <CodeLine onLineSelect={this.onLineSelect} index={2} link>{'const edad = 36;'}</CodeLine>
-              <br />
-            </div>
-            <div class="grid-item details-panel">
-              {this.renderInfo(this.state.selectedLineIndex)}
-            </div>
+          <div class="grid-item info-item">
+            You can think of a variable as a container in memory where you can put a value.
           </div>
-        </AccordionBody>
-
+          <div class="grid-item code-panel">
+          <CodeLine onLineSelect={this.onLineSelect} index={1} link>{'let name = "Jose";'}</CodeLine>
+          <CodeLine onLineSelect={this.onLineSelect} index={2} link>{'const edad = 36;'}</CodeLine>
+            {/* <CodeLine onLineSelect={this.onLineSelect} index={1} link>{'const counter = 10;'}</CodeLine>
+            <CodeLine onLineSelect={this.onLineSelect} index={2} link>{'for (let i = 0; i< 10; i++)'}</CodeLine>
+            <CodeLine onLineSelect={this.onLineSelect} index={3}>{'{'}</CodeLine>
+            <CodeLine onLineSelect={this.onLineSelect} index={4} link>&nbsp;&nbsp;{'console.log("test");'}</CodeLine>
+            <CodeLine onLineSelect={this.onLineSelect} index={5}>{'}'}</CodeLine> */}
+            <br />
+          </div>
+          <div class="grid-item details-panel">
+            {this.renderInfo(this.state.selectedLineIndex)} 
+          </div>
+        </div>
       </div>
     )
   }
