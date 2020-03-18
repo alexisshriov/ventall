09dcee842e53a1319bb0a28be428e379aad58b5a
alexis <alexis@education.com>
2020-03-18 17:19:27 -0500

    Revert "change components layout"
    This reverts commit e115a52ba468d2d64d7bfcfd5c5d241214920f64.

--- src/components/AccordionBody/AccordionBody.js
+++ src/components/AccordionBody/AccordionBody.js
@@ -5,7 +5,11 @@ class AccordionBody extends React.Component {
   render() {
     return (
         <div className={`accordion-body ${this.props.display?"active":null}`}>
-          {this.props.children}
+          A <b style={{ color: 'green' }}>read-only</b> value is a value that once asigned cannot be changed later on.
+          {'If you try to re-asign it it will throw erros:'}<br /><br />
+          <b style={{ color: 'green' }}>Example:</b> <br />
+          {'const a = 0;'}<br />
+          {'a = 1;'}<span style={{ color: 'brown' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//  this will throw an error.</span>
         </div>
 
     )
--- src/components/MainScreen/MainScreen.js
+++ src/components/MainScreen/MainScreen.js
@@ -1,57 +1,40 @@
 import React from 'react';
+
 import AccordionHeader from '../AccordionHeader/AccordionHeader';
 import AccordionBody from '../AccordionBody/AccordionBody';
-import Line from '../Line/Line';
-import HypCont from '../HyperContent/HyperContent';
 import './MainScreen.css';
 // import { CustomToggle, Accordion, Card } from 'react-bootstrap';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 class MainScreen extends React.Component {
   state = {
-    showBody1: false,
-    expandHypCont1: false
+    showBody: false
   }
 
   onClick = () => {
-    this.setState({showBody1: !this.state.showBody1});
-  }
-
-  expandHyperContent = () => {
-    this.setState({expandHypCont1: !this.state.expandHypCont1})
+    console.log('test')
+    this.setState({showBody: !this.state.showBody});
   }
 
   render() {
     return (
-      <div className="main-screen">
-        <div style={{ display: 'flex' }}>
-          <div style={{ margin: '0 0px' }}>
+        <div className="main-screen">
+          <div style={{ display: 'flex' }}>
+            <div style={{margin:  '0 30px'}}>
             <pre>
-            {/* <h2>For Loop:</h2> */}
-              <div>
-              {<Line offset={5} length={32} display={this.state.expandHypCont1} />}
-                <HypCont expand={this.expandHyperContent}>const</HypCont> counter = 10;<br /><br />
-                <Line offset={3} length={34} />
-                <HypCont>for</HypCont>{'(let i = 0; i< 10; i++)'}<br />
-                {'{'}<br />
-                {'  console.log("test")'}<br />
-                {'}'}<br />
-              </div>
+              <code>
+                <b style={{ color: 'blue' }}>const</b> counter = 10;<br/><br/>
+                {'for(let i = 0; i< 10; i++)'}<br/>
+                {'{'}<br/>
+                {'  console.log("test")'}<br/>
+                {'}'}<br/>
+              </code>
             </pre>
-          </div>
+            </div>
             <div>
-              <div>
-              <AccordionHeader toggleFunction={this.onClick} display={this.state.expandHypCont1}>
-                The <b style={{ color: 'blue' }}>const</b> keyword creates a <b style={{ color: 'green' }} onClick={this.onClick}>read-only</b> reference to a value.  
-              </AccordionHeader>
-              <AccordionBody display={this.state.showBody1 && this.state.expandHypCont1} >
-                  A <b style={{ color: 'green' }}>read-only</b> value is a value that once asigned cannot be changed later on.
-                  {'If you try to re-asign it it will throw erros:'}<br /><br />
-                  <b style={{ color: 'green' }}>Example:</b> <br />
-                  {'const a = 0;'}<br />
-                  {'a = 1;'}<span style={{ color: 'brown' }}>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;//  this will throw an error.</span>
-              </AccordionBody>
-              </div>        
+              <AccordionHeader toggleFunction={this.onClick}/>
+              <AccordionBody display={this.state.showBody} />
+              {/* <AccordionBody display={this.state.showBody} level = {3} /> */}
             </div>
           </div>
 
