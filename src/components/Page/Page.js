916e5ff99f83ea4153f1cc0cfe03f197eed1159e
alexis <alexis@education.com>
2020-03-18 18:13:28 -0500

    added two more code lines for example

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -2,7 +2,7 @@ import React from 'react';
 import CodeLine from '../CodeLine/CodeLine';
 import AccordionHeader from '../AccordionHeader/AccordionHeader';
 import AccordionBody from '../AccordionBody/AccordionBody';
-import LineInfo from '../CodeLine/CodeLine';
+// import LineInfo from '../CodeLine/CodeLine';
 import './Page.scss';
 
 class Page extends React.Component {
@@ -30,7 +30,7 @@ class Page extends React.Component {
       case 1:
         return(       
           <div className="line-info">
-            The const keyword creates a <b style={{ color: '#15aabf' }} onClick={this.expandHyperContent}>read-only</b> reference to a value.
+            The const keyword creates a <span className="highlighted" onClick={this.expandHyperContent}>read-only</span> reference to a value.
           </div>      
           /* <div className="line-info">
             A <b onClick={this.expandHyperContent} style={{ color: '#15aabf' }}>read-only</b> value is a value that once asigned cannot be changed later on.
@@ -43,9 +43,21 @@ class Page extends React.Component {
       case 2: 
         return(
           <div className="line-info">
-            {'this is another explanation about  a secondary line'}
+            <span className="highlighted" onClick={this.expandHyperContent}>let</span> is used to declare variables that allow you to change their value later on.
           </div>
         )
+      case 3: 
+      return(
+        <div className="line-info">
+          {'since you declared this variable with let, you can re-asign its value any time you want'}
+        </div>
+      )
+      case 4: 
+      return(
+        <div className="line-info">
+          {'console.log(variable) prints out the value the the variable pass inside the parentheses'}
+        </div>
+      )
     }
   }
   
@@ -65,6 +77,9 @@ class Page extends React.Component {
             <div class="grid-item">
               <CodeLine onLineSelect={this.onLineSelect} index={1} link>{'let name = "Jose";'}</CodeLine>
               <CodeLine onLineSelect={this.onLineSelect} index={2} link>{'const edad = 36;'}</CodeLine>
+              <CodeLine onLineSelect={this.onLineSelect} index={3} link>{'edad = 36 + 5;'}</CodeLine>
+              <CodeLine onLineSelect={this.onLineSelect} index={4} link>{'console.log(edad);'}</CodeLine>
+
               <br />
             </div>
             <div class="grid-item detail-container">
