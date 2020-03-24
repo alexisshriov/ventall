f4476cfefbc80e07348d900d4b66127cf0aca499
alexis <alexis@education.com>
2020-03-24 18:43:46 -0500

    added for line

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -76,6 +76,7 @@ const Page = () => {
             <CodeLine onLineSelect={onLineSelect} index={1} link>{'let name = "Jose";'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={2} link>{'const edad = 36;'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={3} link>{'edad = 36 + 5;'}</CodeLine>
+            <CodeLine onLineSelect={onLineSelect} index={3} link>{'for (let i = 0; i< 100; i++)'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={4} link>{'console.log(edad);'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={4} link>{'edad = 18;'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={4} link>{'test line;'}</CodeLine>
