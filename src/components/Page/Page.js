1ebf5534e340e31b849231da1a12552f2953b726
alexis <alexis@education.com>
2020-04-24 07:32:26 -0500

    minus example

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -76,6 +76,7 @@ const Page = () => {
             <CodeLine onLineSelect={onLineSelect} index={1} link>{'let name = "Jose";'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={2} link>{'const edad = 36;'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={3} link>{'edad = 36 + 5;'}</CodeLine>
+            <CodeLine onLineSelect={onLineSelect} index={3} link>{'edad = edad - + 11;'}</CodeLine>
             <br />
           </div>
           <div class="grid-item detail-container">
