9cd3beb8dd4ff0439625b8a3b57af99b019428a5
alexis <alexis@education.com>
2020-05-26 06:58:07 -0500

    division example

--- src/components/Page/Page.js
+++ src/components/Page/Page.js
@@ -77,6 +77,7 @@ const Page = () => {
             <CodeLine onLineSelect={onLineSelect} index={2} link>{'const edad = 36;'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={3} link>{'edad = 36 + 5;'}</CodeLine>
             <CodeLine onLineSelect={onLineSelect} index={3} link>{'edad = 35 % 5;'}</CodeLine>
+            <CodeLine onLineSelect={onLineSelect} index={3} link>{'edad = edad / 5;'}</CodeLine>
   
         
             <br />
